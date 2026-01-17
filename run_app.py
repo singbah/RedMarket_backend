4e20041297da22801755c3224cff9ed89905e20b
John P. Singbah <singbahjohn@gmail.com>
2026-01-17 19:54:51 +0000

    Add some stuffs

--- run_app.py
+++ run_app.py
@@ -9,5 +9,6 @@ def send_photo(filename):
 
 if __name__=="__main__":
     with app.app_context():
+        # db.drop_all()
         db.create_all()
     app.run(debug=True, host="localhost", port=8080)
